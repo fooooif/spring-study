ca98fe8764b129b17c08dc71ba9a930240e85eaa
유상민 <yoosangmin@yusangmin-ui-MacBookPro.local>
2021-07-05 21:52:41 +0900

    bean 찾기

--- core/src/test/java/com/example/demo/beanfind/ApplicationContextInfoTest.java
+++ core/src/test/java/com/example/demo/beanfind/ApplicationContextInfoTest.java
@@ -26,12 +26,11 @@ public class ApplicationContextInfoTest {
     @DisplayName("애플리케이 빈 출력하기")
     void findApplicationBean(){
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
-            
+
 
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean(beanDefinitionName);
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
-
             //Role ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
             //Role ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
             if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
