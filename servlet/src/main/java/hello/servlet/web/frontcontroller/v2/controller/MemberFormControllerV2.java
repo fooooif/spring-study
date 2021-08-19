02a5d6229c11c541a1fd2c8eeb20b01d20588246
fooooif <qpqh1@naver.com>
2021-08-19 15:45:01 +0900

    Controller V2

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.frontcontroller.v2.controller;
+
+import hello.servlet.web.frontcontroller.Myview;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberFormControllerV2 implements ControllerV2 {
+
+    @Override
+    public Myview process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Myview myview = new Myview("/WEB-INF/views/new-form.jsp");
+        return myview;
+    }
+}
