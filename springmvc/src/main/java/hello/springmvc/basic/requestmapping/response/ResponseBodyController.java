da569a67db581e52651037b07e8d150b94a97962
fooooif <qpqh1@naver.com>
2021-08-24 17:35:19 +0900

    Thymleaf

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/response/ResponseBodyController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/response/ResponseBodyController.java
@@ -3,15 +3,19 @@ package hello.springmvc.basic.requestmapping.response;
 
 import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpLogging;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 //@Controller
@@ -29,7 +33,8 @@ public class ResponseBodyController {
     public ResponseEntity<String> responseBodyV2() throws IOException {
         return new ResponseEntity<>("ok", HttpStatus.OK);
     }
-//    @ResponseBody
+
+    //    @ResponseBody
     @GetMapping("/response-body-string-v3")
     public String responseBodyV3() {
         return "ok";
@@ -40,9 +45,10 @@ public class ResponseBodyController {
 
         HelloData helloData = new HelloData();
 
-        ResponseEntity<HelloData> responseEntity = new ResponseEntity<HelloData>(helloData,HttpStatus.OK);
+        ResponseEntity<HelloData> responseEntity = new ResponseEntity<HelloData>(helloData, HttpStatus.OK);
         return responseEntity;
     }
+
     @ResponseStatus(HttpStatus.OK)
 //    @ResponseBody
     @GetMapping("response-body-json-v2")
@@ -51,5 +57,29 @@ public class ResponseBodyController {
         HelloData helloData = new HelloData();
 
         return helloData;
+    }
+
+    @ResponseBody
+    @GetMapping("/aaaaa")
+    public ResponseEntity<List<HelloData>> aa() {
+        List<HelloData> helloDataList = new ArrayList<>();
+
+        helloDataList.add(new HelloData());
+        helloDataList.add(new HelloData());
+
+        ResponseEntity<List<HelloData>> e = new ResponseEntity<List<HelloData>>(helloDataList, HttpStatus.OK);
+
+        return e;
+    }
+    @ResponseBody
+    @GetMapping("/aaaaaa")
+    public List<HelloData> aaa() {
+        List<HelloData> helloDataList = new ArrayList<>();
+
+        helloDataList.add(new HelloData());
+        helloDataList.add(new HelloData());
+
+
+        return helloDataList;
     }
 }
